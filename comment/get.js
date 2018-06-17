10ef19fdcda1aac9a1256ee46db94594d1a78d60
datamaker <wjdqls1342@gmail.com>
2018-06-17 13:48:27 +0900

    comment issueId 으로 출력

--- comment/get.js
+++ comment/get.js
@@ -3,24 +3,20 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.DYNAMODB_TABLE_COMMENT,
+};
 
 module.exports.get = (event, context, callback) => {
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE_COMMENT,
-    Key: {
-      id: event.pathParameters.id,
-    },
-  };
-
-  // fetch comment from the database
-  dynamoDb.get(params, (error, result) => {
+  // fetch all comments from the database
+  dynamoDb.scan(params, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the article item.',
+        body: 'Couldn\'t fetch the comments.',
       });
       return;
     }
@@ -29,7 +25,7 @@ module.exports.get = (event, context, callback) => {
     const response = {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin' : '*' },
-      body: JSON.stringify(result.Item),
+      body: JSON.stringify(result.Items.filter(item => item.issueId === event.pathParameters.id)),
     };
     callback(null, response);
   });
